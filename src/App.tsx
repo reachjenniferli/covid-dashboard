2e41068c534dbc4a2fb7ce85f72fca1cc9b219a6
Jennifer Li <jenniferli7437@gmail.com>
2025-06-26 16:27:08 -0400

    Finish app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import SelectCountry from "./components/select-country";
+import DisplayStatistics from "./components/display-statistics";
+import { useState } from "react";
+
 function App() {
+  const [countryCode, setCountryCode] = useState<string | null>(null);
+
   return (
-    <div className="flex justify-center items-center text-3xl min-h-screen">
-      Covid Dashboard
+    <div className="flex flex-col justify-between items-center min-h-screen container space-y-4 max-w-4xl py-10">
+      <SelectCountry setCountryCode={setCountryCode} />
+      <DisplayStatistics countryCode={countryCode} />
     </div>
   );
 }
---
+++ src/components/display-statistics.tsx
@@ -0,0 +1,51 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { useEffect, useState } from "react";
+import { type CovidData } from "@/data/types";
+import { fetchCovidData } from "@/data/api";
+
+const STATISTICS: (keyof CovidData)[] = ["confirmed", "active", "recovered"];
+
+type DisplayStatisticsProps = {
+  countryCode: string | null;
+};
+
+const DisplayStatistics = ({ countryCode }: DisplayStatisticsProps) => {
+  const [covidData, setCovidData] = useState<CovidData | null>(null);
+
+  useEffect(() => {
+    console.log("countryCode", countryCode);
+    if (countryCode) {
+      fetchCovidData(countryCode).then((data) => setCovidData(data));
+    }
+  }, [countryCode]);
+
+  if (!countryCode) {
+    return null;
+  }
+
+  return (
+    <div className="flex w-full justify-between flex-col sm:flex-row gap-5">
+      {STATISTICS.map((statistic) => (
+        <Card className="w-full" key={statistic}>
+          <CardHeader>
+            <CardTitle className="capitalize">{statistic}</CardTitle>
+          </CardHeader>
+          <CardContent className="flex justify-right items-center gap-2">
+            <Avatar>
+              <AvatarImage src={covidData?.countryFlag} />
+              <AvatarFallback>
+                {covidData?.countryCode.toUpperCase()}
+              </AvatarFallback>
+            </Avatar>
+            <div className="text-2xl">
+              {covidData?.[statistic]?.toLocaleString()}
+            </div>
+          </CardContent>
+        </Card>
+      ))}
+    </div>
+  );
+};
+
+export default DisplayStatistics;
